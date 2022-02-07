f0dcae280971ffbc4717c537ddfb9fc4037c36eb
Younji Woo <younjiwoo@Younjis-MacBookPro.local>
2022-02-07 15:57:48 +0900

    increment / decrement buttons work

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,35 @@
+import { useSelector, useDispatch } from 'react-redux';
+
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+	const dispatch = useDispatch();
 
-  return (
-    <main className={classes.counter}>
-      <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
-    </main>
-  );
+	const counter = useSelector((state) => state.counter);
+
+	const incrementHandler = () => {
+		dispatch({ type: 'increment' });
+	};
+
+	const decrementHandler = () => {
+		dispatch({ type: 'decrement' });
+	};
+
+	const toggleCounterHandler = () => {};
+
+	console.log('counter: ', counter);
+
+	return (
+		<main className={classes.counter}>
+			<h1>Redux Counter</h1>
+			<div className={classes.value}>{counter}</div>
+			<div>
+				<button onClick={incrementHandler}>Increment</button>
+				<button onClick={decrementHandler}>Decrement</button>
+			</div>
+			<button onClick={toggleCounterHandler}>Toggle Counter</button>
+		</main>
+	);
 };
 
 export default Counter;
--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,18 @@
-// create a store, not subscribe.
 import { createStore } from 'redux';
 
 const counterReducer = (state = { counter: 0 }, action) => {
 	if (action.type === 'increment') {
 		return {
-			state: state.counter + 1,
+			counter: state.counter + 1,
 		};
 	}
+
 	if (action.type === 'decrement') {
 		return {
-			state: state.counter - 1,
+			counter: state.counter - 1,
 		};
 	}
+
 	return state;
 };
 
